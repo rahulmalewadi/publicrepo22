ab3c75ece44b3436e9c51ef2cd16c1d024cfbbcd
rmalewad <rmalewad@LIN51013709.corp.capgemini.com>
2021-07-07 13:12:26 +0530

    first commit

---
+++ devops222/src/test/java/github/devops222/AppTest.java
@@ -0,0 +1,13 @@
+package github.devops222;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
